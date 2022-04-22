2a5357afa1cb3a68e31f44445c71052d13f68be6
lucaspetti <luc_petti@hotmail.com>
2022-04-22 22:16:36 +0200

    Update rails_admin initializer

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,42 +1,39 @@
+# frozen_string_literal: true
+
 RailsAdmin.config do |config|
+  config.main_app_name = ['Artist Roster']
+
   config.asset_source = :webpacker
 
-  ### Popular gems integration
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.current_user_method(&:current_user)
 
-  ## == CancanCan ==
-  # config.authorize_with :cancancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/railsadminteam/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
+  creatable = ['Artist', 'MonthStreamingImport', 'Playlist', 'PlaylistDataImport', 'Release']
+  editable = ['Artist', 'Playlist', 'Release']
 
   config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
-    new
-    export
-    bulk_delete
+    dashboard
+    index
     show
-    edit
-    delete
-    show_in_app
+    export
 
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+    new do
+      only creatable
+    end
+
+    edit do
+      only editable
+    end
+
+    bulk_delete do
+      only creatable
+    end
+
+    delete do
+      only creatable
+    end
   end
 end
