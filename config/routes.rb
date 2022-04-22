4d10dc7cacf54cd0531a152ad5ce5e229be79dec
lucaspetti <luc_petti@hotmail.com>
2022-04-22 15:35:03 +0200

    Add seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'rails_admin/main#dashboard'
 end
