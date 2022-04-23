c167176e75e3f3b4194c318118e02202cbb6628e
lucaspetti <luc_petti@hotmail.com>
2022-04-23 09:52:59 +0200

    Update application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def streams_data_html(data)
+    "<strong>#{data.month}/#{data.year}</strong> </br>Listeners: #{data.listeners} </br> Streams: #{data.streams} </br> Followers: #{data.followers} </br></br>"
+  end
 end
