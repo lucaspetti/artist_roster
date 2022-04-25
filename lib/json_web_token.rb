d9719e5e8c3ed2f75b4b2ae17aa723d31e171ded
lucaspetti <luc_petti@hotmail.com>
2022-04-25 17:50:06 +0200

    Add fallback to env variable for rails secret key

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,14 +3,18 @@
 class JsonWebToken
   class << self
     def encode(payload)
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, secret_key_base)
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, secret_key_base)[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
+    end
+
+    def secret_key_base
+      Rails.application.secrets.secret_key_base || ENV['SECRET_KEY_BASE']
     end
   end
 end
