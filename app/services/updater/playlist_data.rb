f66cfaeb38524b2954b37740bf450db99d281a2d
lucaspetti <luc_petti@hotmail.com>
2022-06-05 11:31:50 +0200

    Do not raise error when playlist data for last month was not found

--- app/services/updater/playlist_data.rb
+++ app/services/updater/playlist_data.rb
@@ -7,9 +7,13 @@ module Updater
     end
 
     def update!
+      last_month = Date.today.last_month
+      month = last_month.month
+      year = last_month.year
+
       playlist = Playlist.find_by_name!(@playlist_spotify_data.name)
-      playlist_data = PlaylistDatum.where(playlist: playlist).last
-      raise 'Playlist Data not found' if playlist_data.nil?
+      playlist_data = PlaylistDatum.where(playlist: playlist, month: month, year: year).last
+      return if playlist_data.nil?
 
       playlist_data.update!(followers: @playlist_spotify_data.followers['total'])
       playlist.update!(mapped_attributes)
--- spec/services/updater/playlist_data_spec.rb
+++ spec/services/updater/playlist_data_spec.rb
@@ -2,8 +2,6 @@
 
 require 'rails_helper'
 
-PlaylistSpotifyData = Struct.new(:name, :id, :followers, :description)
-
 RSpec.describe Updater::PlaylistData do
   let(:playlist) { create :playlist }
   let(:playlist_spotify_data) do
@@ -19,6 +17,8 @@ RSpec.describe Updater::PlaylistData do
   let(:update) { spotify_playlist_data_updater.update! }
 
   describe '#update!' do
+    before { Timecop.freeze(Time.local(2020, 9, 1, 12, 0, 0)) }
+
     context 'playlist not found' do
       it 'raises an error if playlist was not found' do
         playlist.delete
@@ -27,17 +27,12 @@ RSpec.describe Updater::PlaylistData do
 
     end
 
-    context 'playlist data not found' do
-      it 'raises an error if playlist data was not found' do
-        expect { update }.to raise_error('Playlist Data not found')
+    context 'success' do
+      let!(:playlist_data) do
+        create :playlist_datum, playlist: playlist, month: 8, year: 2020
       end
 
-    end
-
-    context 'success' do
-      let!(:playlist_data) { create :playlist_datum, playlist: playlist }
-
-      it 'updates last playlist data found by name' do
+      it 'updates last months playlist data found by name' do
         update
         expect(playlist.reload.spotify_id).to eq(playlist_spotify_data.id)
         expect(playlist.reload.description).to eq(playlist_spotify_data.description)
